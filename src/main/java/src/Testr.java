9994661dc5ad1a7a0cd519ff873c2711eb88e59c
beton726 <909043377fas>
2021-09-12 04:22:09 +0300

    Доработка моего алгоритма.

--- src/main/java/src/Testr.java
+++ src/main/java/src/Testr.java
@@ -4,6 +4,8 @@ public class Testr {
 
     public static void alg() {
         System.out.println("Слздание алгоритма.");
+        System.out.println("Слздание алгоритма 2.");
+        System.out.println("Слздание алгоритма 3.");
     }
 
 }

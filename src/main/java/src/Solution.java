21ae596d192090c43ecd4a126c4a54933e7f4655
beton726 <909043377fas>
2021-09-12 04:07:19 +0300

    Новый проект.

---
+++ src/main/java/src/Solution.java
@@ -0,0 +1,33 @@
+package src;
+
+import org.junit.Test;
+
+public class Solution {
+
+    int[] m = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    @Test
+    public void testMethod() {
+
+        System.out.println("Позиция: " + binarySearch(m, 10));
+
+    }
+
+   private int binarySearch(int[] m, int x) {
+        int i = 0;
+        int j = m.length;
+
+        while (i != j) {
+            int del = (i + j) / 2;
+
+            if(x == m[del])
+                return del;
+            if (x < m[del])
+                j = del;
+            else
+                i = del + 1;
+        }
+        return -1;
+   }
+
+}
